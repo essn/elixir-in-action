ead3c560c825efe15e1eae32c7f73e20b62bc288
Jesse Pledger <jpledger@datalot.com>
2021-12-29 12:09:03 -0500

    Server back to GenServer with timeout

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,49 +1,45 @@
 defmodule Todo.Server do
-  use Agent, restart: :temporary
+  use GenServer, restart: :temporary
+
+  @expiry_idle_timeout :timer.seconds(10)
 
   # Client
 
   def start_link(name) do
-    Agent.start_link(
-      fn ->
-        IO.puts("Starting to-do server for #{name}")
-        {name, Todo.Database.get(name) || Todo.List.new()}
-      end,
-      name: via_tuple(name)
-    )
+    IO.puts("Starting to-do server for #{name}.")
+
+    GenServer.start_link(Todo.Server, name, name: via_tuple(name))
   end
 
   def add_entry(todo_server, new_entry) do
-    Agent.cast(todo_server, fn {name, todo_list} ->
-      new_list = Todo.List.add_entry(todo_list, new_entry)
-      Todo.Database.store(name, new_list)
-      {name, new_list}
-    end)
+    GenServer.cast(todo_server, {:add_entry, new_entry})
   end
 
   def entries(todo_server, date) do
-    Agent.get(todo_server, fn {_name, todo_list} ->
-      Todo.List.entries(todo_list, date)
-    end)
+    GenServer.call(todo_server, {:entries, date})
   end
 
   # Server
 
+  @impl true
   def init(name) do
-    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}, @expiry_idle_timeout}
   end
 
+  @impl true
   def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
     new_list = Todo.List.add_entry(todo_list, new_entry)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
+  @impl true
   def handle_call({:entries, date}, _, {name, todo_list}) do
     {
       :reply,
       Todo.List.entries(todo_list, date),
-      {name, todo_list}
+      {name, todo_list},
+      @expiry_idle_timeout
     }
   end
 
