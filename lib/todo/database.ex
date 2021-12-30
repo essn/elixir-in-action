9ae7382b93c927b762c3b6529914edaa2bca4f39
Jesse Pledger <jpledger@datalot.com>
2021-12-30 10:09:55 -0500

    Make database distributable

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -3,7 +3,11 @@ defmodule Todo.Database do
   @dialyzer {:nowarn_function, child_spec: 1}
 
   def child_spec(_) do
-    File.mkdir_p!(@db_folder)
+    [name_prefix, _] = "#{node()}" |> String.split("@")
+
+    db_folder = "#{@db_folder}/#{name_prefix}"
+
+    File.mkdir_p!(db_folder)
 
     :poolboy.child_spec(
       __MODULE__,
@@ -12,11 +16,20 @@ defmodule Todo.Database do
         worker_module: Todo.DatabaseWorker,
         size: 3
       ],
-      [@db_folder]
+      [db_folder]
     )
   end
 
   def store(key, data) do
+    {_results, bad_nodes} =
+      :rpc.multicall(__MODULE__, :store_local, [key, data], :timer.seconds(5))
+
+    Enum.each(bad_nodes, &IO.puts("Store failed on node #{&1}"))
+
+    :ok
+  end
+
+  def store_local(key, data) do
     :poolboy.transaction(
       __MODULE__,
       fn worker_pid -> Todo.DatabaseWorker.store(worker_pid, key, data) end
